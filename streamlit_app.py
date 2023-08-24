675d37b52b840f9d06c393b52eaee52c181d5b04
ShahVishs <121416561+ShahVishs@users.noreply.github.com>
2023-08-24 11:48:54 +0530

    streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -83,7 +83,7 @@ chat_history=[]
 def conversational_chat(query):
     result = qa({"question": query, "chat_history": chat_history})
     chat_history.append((question, result["answer"]))
-    st.session_state.history.append((query, result["answer"]))
+    # st.session_state.history.append((query, result["answer"]))
     return result["answer"]
     
 with container:
