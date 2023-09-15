5c7819e6b02ece835aead965a47e243b8aad6cee
ShahVishs <121416561+ShahVishs@users.noreply.github.com>
2023-09-15 15:12:32 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -232,9 +232,9 @@ with container:
         user_name = st.text_input("Your name:")
         if user_name:
             st.session_state.user_name = user_name
-            
+    
     with st.form(key='my_form', clear_on_submit=True):
-        user_input = st.text_input("Query:", placeholder="Type your question here (:", key='input')
+        user_input = st.text_input("Query:", placeholder="Type your question here (:")
         submit_button = st.form_submit_button(label='Send')
     
     if submit_button and user_input:
